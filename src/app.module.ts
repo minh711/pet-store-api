0022b09f50f432c48023cd4cc04bc9bf8b68ccf7
minh711 <duyminhtruong711@gmail.com>
2024-02-05 12:42:58 +0700

    Update driver.
    npm install @apollo/server
    
    npm install @nestjs/apollo

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { PetsModule } from './pets/pets.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { ApolloDriver } from '@nestjs/apollo';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
+      driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
     PetsModule,
--- src/pets/pets.module.ts
+++ src/pets/pets.module.ts
@@ -3,6 +3,6 @@ import { PetsService } from './pets.service';
 import { PetsResolver } from './pets.resolver';
 
 @Module({
-  providers: [PetsService, PetsResolver]
+  providers: [PetsService, PetsResolver],
 })
 export class PetsModule {}
