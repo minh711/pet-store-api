de0fc84d2816fd08778f3b8e4e5a265d6498fbed
minh711 <duyminhtruong711@gmail.com>
2024-02-05 18:36:46 +0700

    TypeORM module configuration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,19 @@ import { PetsModule } from './pets/pets.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { ApolloDriver } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: ':memory:',
+      entities: ['dist/**/*/*.entity{.ts,.js}'],
+      synchronize: true, // just for development, should not be use in production
     }),
     PetsModule,
   ],
