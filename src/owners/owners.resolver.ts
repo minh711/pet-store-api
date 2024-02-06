4f9805496a1eb00fb42cb2e8e64b03ee3091e321
minh711 <duyminhtruong711@gmail.com>
2024-02-06 16:19:25 +0700

    Owners and pets relationship functions.

--- src/owners/owners.resolver.ts
+++ src/owners/owners.resolver.ts
@@ -2,7 +2,6 @@ import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { OwnersService } from './owners.service';
 import { Owner } from './entities/owner.entity';
 import { CreateOwnerInput } from './dto/create-owner.input';
-import { UpdateOwnerInput } from './dto/update-owner.input';
 
 @Resolver(() => Owner)
 export class OwnersResolver {
@@ -21,15 +20,5 @@ export class OwnersResolver {
   @Query(() => Owner, { name: 'owner' })
   findOne(@Args('id', { type: () => Int }) id: number) {
     return this.ownersService.findOne(id);
-  }
-
-  @Mutation(() => Owner)
-  updateOwner(@Args('updateOwnerInput') updateOwnerInput: UpdateOwnerInput) {
-    return this.ownersService.update(updateOwnerInput.id, updateOwnerInput);
-  }
-
-  @Mutation(() => Owner)
-  removeOwner(@Args('id', { type: () => Int }) id: number) {
-    return this.ownersService.remove(id);
   }
 }
--- src/pets/pets.service.ts
+++ src/pets/pets.service.ts
@@ -3,10 +3,15 @@ import { Pet } from './pet.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreatePetInput } from './dto/create-pet.input';
+import { OwnersService } from '../owners/owners.service';
+import { Owner } from '../owners/entities/owner.entity';
 
 @Injectable()
 export class PetsService {
-  constructor(@InjectRepository(Pet) private petsRepository: Repository<Pet>) {}
+  constructor(
+    @InjectRepository(Pet) private petsRepository: Repository<Pet>,
+    private ownersService: OwnersService,
+  ) {}
 
   async createPet(createPetInput: CreatePetInput): Promise<Pet> {
     const newPet = this.petsRepository.create(createPetInput); // newPet = new Pet()
@@ -20,5 +25,9 @@ export class PetsService {
 
   findOne(id: number): Promise<Pet> {
     return this.petsRepository.findOneOrFail({ where: { id } });
+  }
+
+  getOwner(ownerId: number): Promise<Owner> {
+    return this.ownersService.findOne(ownerId);
   }
 }
